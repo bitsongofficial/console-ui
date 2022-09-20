7cdcd25fd16057c0b5e0abe693551ac900e8e904
DavideSegullo <davidesegullo@gmail.com>
2022-09-20 09:34:26 +0200

    feat: :sparkles: add fantokens search

--- src/pages/Fantokens.vue
+++ src/pages/Fantokens.vue
@@ -2,14 +2,22 @@
 import { btsgAssets, btsgStakingCoin } from "@/configs"
 import { TableColumn } from "@/models"
 import { FanToken } from "@bitsongjs/client/dist/codec/bitsong/fantoken/v1beta1/fantoken"
-import { onMounted } from "vue"
+import { onMounted, ref } from "vue"
 import { fromBaseToDisplay } from "@/utils"
 import { Coin } from "@bitsongjs/client/dist/codec/cosmos/base/v1beta1/coin"
+import { useVueFuse } from "vue-fuse"
 import useAuth from "@/store/auth"
 import useFantoken from "@/store/fantoken"
 
 const fantokenStore = useFantoken()
 const authStore = useAuth()
+
+const { search, runSearch, results, noResults } = useVueFuse(
+	fantokenStore.fantokens,
+	{
+		keys: ["metaData.name", "metaData.symbol", "minter"],
+	}
+)
 
 const columns: TableColumn[] = [
 	{
@@ -68,6 +76,13 @@ const pagination = {
 onMounted(() => {
 	fantokenStore.loadFantokens()
 })
+
+const onSearchText = (value: string | number | null) => {
+	if (value) {
+		search.value = `${value}`
+		runSearch(search.value)
+	}
+}
 </script>
 
 <template>
@@ -75,20 +90,31 @@ onMounted(() => {
 		<div class="col-auto">
 			<div class="row">
 				<div class="col">
-					<h4 class="q-mb-lg q-mt-none text-bold">Fantokens</h4>
+					<h4 class="q-mb-lg q-mt-none text-bold">
+						Fantokens {{ noResults }} {{ results.length }}
+					</h4>
 				</div>
 			</div>
 		</div>
 
 		<div class="col-auto">
 			<q-table
-				:rows="fantokenStore.fantokens"
+				:rows="noResults || search.length === 0 ? fantokenStore.fantokens : results"
 				:loading="fantokenStore.loading"
 				:columns="columns"
 				:pagination="pagination"
 				row-key="address"
 			>
-				<template v-slot:top-right> </template>
+				<template v-slot:top-right>
+					<q-input
+						class="col-12 col-md-4 col-lg-3"
+						label="Search"
+						dense
+						filled
+						v-model="search"
+						@update:model-value="onSearchText"
+					/>
+				</template>
 			</q-table>
 		</div>
 	</q-page>
